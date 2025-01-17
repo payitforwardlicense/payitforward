8a84c01b8e92c72fd230f443c97a639236ebd19d
space <github@arzynik.com>
2025-01-16 21:50:23 -0800

    sitemap test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import mdx from '@astrojs/mdx'
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
