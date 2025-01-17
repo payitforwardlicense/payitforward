3a55c6897d16612c374a641a0c13eae7bccb0e97
space <github@arzynik.com>
2025-01-17 15:33:04 -0800

    added shop items

--- src/content.config.js
+++ src/content.config.js
@@ -1,8 +1,8 @@
 import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
-const blog = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/blog' }),
+const posts = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/posts' }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
@@ -10,4 +10,17 @@ const blog = defineCollection({
   })
 })
 
-export const collections = { blog }
+const shop = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/shop' }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    slug: z.string(),
+    image: z.string(),
+    price: z.number(),
+    category: z.string(),
+    url: z.string(),
+  })
+})
+
+export const collections = { posts, shop }
