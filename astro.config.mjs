7a6df8022059c27e710c26b5222e86c36996f0eb
space <github@arzynik.com>
2025-01-19 18:38:11 -0800

    themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,4 +16,56 @@ export default defineConfig({
     clientPrerender: true,
     svg: true,
   },
+  markdown: {
+    shikiConfig: {
+      wrap: true,
+      // themes: {
+      //   light: 'github-dark',
+      //   dark: 'github-dark',
+      // },
+      theme: 
+        `andromeeda`
+        // `aurora-x`
+        // `ayu-dark`
+        // `catppuccin-frappe`
+        // `catppuccin-macchiato`
+        // `catppuccin-mocha`
+        // `dark-plus`
+        // `dracula`
+        // `dracula-soft`
+        // `everforest-dark`
+        // `github-dark`
+        // `github-dark-default`
+        // `github-dark-dimmed`
+        // `github-dark-high-contrast`
+        // `houston`
+        // `kanagawa-dragon`
+        // `kanagawa-wave`
+        // `laserwave`
+        // `material-theme`
+        // `material-theme-darker`
+        // `material-theme-ocean`
+        // `material-theme-palenight`
+        // `min-dark`
+        // `monokai`
+        // `night-owl`
+        // `nord`
+        // `one-dark-pro`
+        // `plastic`
+        // `poimandres`
+        // `red`
+        // `rose-pine`
+        // `rose-pine-moon`
+        // `slack-dark`
+        // `solarized-dark`
+        // `synthwave-84`
+        // `tokyo-night`
+        // `vesper`
+        // `vitesse-black`
+        // `vitesse-dark`
+    },
+  },
 })
+
+//'rose-pine-dawn
+//ayu-dark
