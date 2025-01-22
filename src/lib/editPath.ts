bba09b571ab89b23f0c8d262fac1d08e23816176
space <github@arzynik.com>
2025-01-21 21:31:01 -0800

    path

--- src/lib/editPath.ts
+++ src/lib/editPath.ts
@@ -3,7 +3,11 @@ import path from 'node:path'
 
 export const editPath = (filePath: string) => {
   const base = path.dirname(path.join(filePath, '../../'))
-  const currentPath = base === '.' ? filePath : filePath.replace(base, '')
-  const improveFile = `${repository.url}/blob/main/${currentPath}`
+  console.log(filePath)
+  const currentPath = base === '.' ? filePath : filePath
+    .replaceAll(base, '')
+    .replaceAll('dist', 'src')
+    .replaceAll('.astro.mjs', '.astro')
+  const improveFile = `${repository.url}/blob/main${currentPath}`
   return improveFile
 }
